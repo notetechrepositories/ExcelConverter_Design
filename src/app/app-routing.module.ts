0a7bbb8ac4a7bd58618202aae8d175097c361289
athulks@notetech.com <athulks@notetech.com>
2024-02-27 18:37:39 +0530

    UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TutorialPageComponent } from './tutorial-page/tutorial-page.component';
+import { LoginComponent } from './login/login.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:"home",component:HomeComponent},
-  {path:"tutorial-page", component:TutorialPageComponent}
+  {path:"tutorial-page", component:TutorialPageComponent},
+  {path:"login",component:LoginComponent},
+  {path:"admin",component:AdminDashboardComponent}
 
 ];
 
